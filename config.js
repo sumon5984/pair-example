91315340ab10a9cbd9cf2557369833f8064b73e9
sumon5984 <sbot7603@gmail.com>
2025-06-05 12:34:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
   PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  EMAIL: process.env.EMAIL || "sbot7603@gmail.com",
+  PASS: process.env.PASS || "sumon@983655",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
